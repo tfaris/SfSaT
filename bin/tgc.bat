87d48ffaa063f84964a02e776dee4cdc3450f2da
tfaris <ta.faris@gmail.com>
2015-12-18 14:54:39 -0500

    Initial commit.

---
+++ bin/tgc.bat
@@ -0,0 +1,4 @@
+@echo off
+REM Open the TortoiseGit commit dialog in the current directory.
+REM tfaris 07.16.14
+"C:\Program Files\TortoiseGit\bin\TortoiseGitProc.exe" /command:commit
---
+++ bin/tgm.bat
@@ -0,0 +1,4 @@
+@echo off
+REM Opens the TortoiseGit merge dialog in the current directory.
+REM tfaris 03.06.14
+"C:\Program Files\TortoiseGit\bin\TortoiseGitProc.exe" /command:merge %*

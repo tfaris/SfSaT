4481ad127872903f9be30e520ac7de1179041e6c
tfaris <ta.faris@gmail.com>
2016-01-13 14:37:20 -0500

    Pass cmd arguments along through tgc script.

--- bin/tgc.bat
+++ bin/tgc.bat
@@ -1,4 +1,4 @@
 @echo off
 REM Open the TortoiseGit commit dialog in the current directory.
 REM tfaris 07.16.14
-"C:\Program Files\TortoiseGit\bin\TortoiseGitProc.exe" /command:commit
+"C:\Program Files\TortoiseGit\bin\TortoiseGitProc.exe" /command:commit %*

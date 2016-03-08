2d0c243214af5fb35d1ba5842f93b3c7d12caa90
tfaris <ta.faris@gmail.com>
2016-03-08 12:03:04 -0500

    tg log command assumes single argument as revision to show log for.
    If a single argument is passed to tg, it uses that as the revision
    range to show logs for. Valid usages:
    
    tg <BRANCH_NAME>
    tg <BRANCH_NAME>~10..<BRANCH_NAME>
    tg <SHA1>
    
    and more. If zero, or more than one argument is passed, they are just
    passed along to the tortoisegit process.
    
    tg.bat now just calls tg. sh required.

--- bin/tg.bat
+++ bin/tg.bat
@@ -1,4 +1,4 @@
 @echo off
 REM Opens the TortoiseGit log dialog in the current directory.
 REM tfaris 03.06.14
-start "" "C:\Program Files\TortoiseGit\bin\TortoiseGitProc.exe" /command:log %*
+sh %~dp0tg %*

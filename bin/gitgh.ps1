7f009195dcb3653540e908a2d432caa1ed560254
tfaris <ta.faris@gmail.com>
2019-04-11 12:16:53 -0400

    Convert gitgh to a powershell script.

--- bin/gitgh.ps1
+++ bin/gitgh.ps1
@@ -1,17 +1,28 @@
-#!/bin/sh
+param(
+    [string] $Ref
+)
 # Pull an issue number from the current branch name and open
 # it in the webbrowser. Only works if github (gh) is the remote origin, 
 # and using the https url.
 # tfaris 12.09.2015
-b=$(git rev-parse --abbrev-ref HEAD)
-ghUrl=$(git config --get remote.origin.url)
-python -c "
+
+if ($Ref)
+{
+    $b = "$(git branch | Select-String $Ref)"
+}
+else
+{
+    $b = $(git rev-parse --abbrev-ref HEAD)
+}
+
+$ghUrl = $(git config --get remote.origin.url)
+
+&python -c "
 import re
 import sys
 import webbrowser
 import time
 gh_url = sys.argv[1]
-gh_url = gh_url.replace('.git', '')
 ref = sys.argv[2]
 print('Origin: %s' % gh_url)
 print('Ref   : %s' % ref)
@@ -26,4 +37,4 @@ except (IndexError, ValueError):
     print('Cannot parse branch name %s as an issue. Opening tree...' % ref)
     url = gh_url + '/tree/' + ref
     webbrowser.open(url)
-" $ghUrl $b
+" "$ghUrl" "$b"
